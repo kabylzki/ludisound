80654968da2bf98d969e04722782e4b5e4006b00
root <taskiran.mikael@hotmail.fr>
2015-01-08 23:59:22 +0100

    #034 - Mise en valeur des Highscores

--- score.php
+++ score.php
@@ -6,6 +6,18 @@ $page = "score";
 
 // Récupère la question
 $scores = $dbh->query('SELECT * FROM score ORDER BY score DESC LIMIT 50')->fetchAll();
+
+$max_stage = $dbh->query('SELECT MAX(stage) FROM score')->fetch();
+$max_level = $dbh->query('SELECT MAX(level) FROM score')->fetch();
+$max_enraged = $dbh->query('SELECT MAX(enraged_used) FROM score')->fetch();
+$max_monster = $dbh->query('SELECT MAX(monster_killed) FROM score')->fetch();
+$max_chest = $dbh->query('SELECT MAX(chest_taken) FROM score')->fetch();
+$max_clock = $dbh->query('SELECT MAX(clock_taken) FROM score')->fetch();
+$max_alcool = $dbh->query('SELECT MAX(alcool_taken) FROM score')->fetch();
+$max_pill = $dbh->query('SELECT MAX(pill_taken) FROM score')->fetch();
+$max_doll = $dbh->query('SELECT MAX(doll_taken) FROM score')->fetch();
+$max_area = $dbh->query('SELECT MAX(area_cleared) FROM score')->fetch();
+$max_question = $dbh->query('SELECT MAX(question) FROM score')->fetch();
 ?>
 <html>
     <head>
@@ -55,22 +67,77 @@ $scores = $dbh->query('SELECT * FROM score ORDER BY score DESC LIMIT 50')->fetch
                             echo "<td><img src='include/images/medal-bronze.png' title='bronze-medal' /></td>";
                         } else {
                             echo "<tr>";
-                            echo "<td>".$i."</td>";
+                            echo "<td>" . $i . "</td>";
                         }
-                        echo "<td>".$score['pseudo']."</td>";
-                        echo "<td>".$score['date']."</td>";
-                        echo "<td class='resp'>".$score['stage']."</td>";
-                        echo "<td class='resp'>".$score['level']."</td>";
-                        echo "<td class='resp'>".$score['enraged_used']."</td>";
-                        echo "<td class='resp'>".$score['monster_killed']."</td>";
-                        echo "<td class='resp'>".$score['chest_taken']."</td>";
-                        echo "<td class='resp'>".$score['clock_taken']."</td>";
-                        echo "<td class='resp'>".$score['alcool_taken']."</td>";
-                        echo "<td class='resp'>".$score['pill_taken']."</td>";
-                        echo "<td class='resp'>".$score['doll_taken']."</td>";
-                        echo "<td class='resp'>".$score['area_cleared']."</td>";
-                        echo "<td class='resp'>".$score['question']."</td>";
-                        echo "<td><b>".$score['score']."</b></td>";
+                        echo "<td>" . $score['pseudo'] . "</td>";
+                        echo "<td>" . $score['date'] . "</td>";
+
+                        if ($score['stage'] == $max_stage[0]) {
+                            echo "<td class='resp'><b>" . $score['stage'] . "</b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['stage'] . "</td>";
+                        }
+
+                        if ($score['level'] == $max_level[0]) {
+                            echo "<td class='resp'><b>" . $score['level'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['level'] . "</td>";
+                        }
+
+                        if ($score['enraged_used'] == $max_enraged[0]) {
+                            echo "<td class='resp'><b>" . $score['enraged_used'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['enraged_used'] . "</td>";
+                        }
+
+                        if ($score['monster_killed'] == $max_monster[0]) {
+                            echo "<td class='resp'><b>" . $score['monster_killed'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['monster_killed'] . "</td>";
+                        }
+
+                        if ($score['chest_taken'] == $max_chest[0]) {
+                            echo "<td class='resp'><b>" . $score['chest_taken'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['chest_taken'] . "</td>";
+                        }
+
+                        if ($score['clock_taken'] == $max_clock[0]) {
+                            echo "<td class='resp'><b>" . $score['clock_taken'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['clock_taken'] . "</td>";
+                        }
+
+                        if ($score['alcool_taken'] == $max_alcool[0]) {
+                            echo "<td class='resp'><b>" . $score['alcool_taken'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['alcool_taken'] . "</td>";
+                        }
+
+                        if ($score['pill_taken'] == $max_pill[0]) {
+                            echo "<td class='resp'><b>" . $score['pill_taken'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['pill_taken'] . "</td>";
+                        }
+
+                        if ($score['doll_taken'] == $max_doll[0]) {
+                            echo "<td class='resp'><b>" . $score['doll_taken'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['doll_taken'] . "</td>";
+                        }
+
+                        if ($score['area_cleared'] == $max_area[0]) {
+                            echo "<td class='resp'><b>" . $score['area_cleared'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['area_cleared'] . "</td>";
+                        }
+
+                        if ($score['question'] == $max_question[0]) {
+                            echo "<td class='resp'><b>" . $score['question'] . "<b></td>";
+                        } else {
+                            echo "<td class='resp'>" . $score['question'] . "</td>";
+                        }
+                        echo "<td><b>" . $score['score'] . "</b></td>";
                         echo "</tr>";
                         $i++;
                     }
